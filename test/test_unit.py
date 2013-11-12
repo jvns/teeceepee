c2e4cfc6ba65e470b86f8038b71508800d8732a3
Julia Evans <julia@jvns.ca>
2013-11-12 15:43:50 -0500

    Reorganize and comment the unit tests better

--- test/test_unit.py
+++ test/test_unit.py
@@ -11,6 +11,7 @@ from mock_listener import MockListener
 
 
 def test_syn():
+    """When we connect, we should send a SYN."""
     listener = MockListener()
     conn = TCPSocket(listener)
     conn.connect("localhost", 80)
@@ -22,6 +23,7 @@ def test_syn():
 
 
 def test_handshake_client():
+    """When we handshake, we should send a SYN and an ACK"""
     listener = MockListener()
     conn = TCPSocket(listener)
     conn.connect("localhost", 80)
@@ -44,27 +46,10 @@ def test_handshake_client():
     assert ack.sprintf("%TCP.flags%") == "A"
 
 
-def create_session(packet_log):
-    listener = MockListener()
-    syn = packet_log[0]
-    listener.source_port = syn.sport - 1
-    conn = TCPSocket(listener)
-    conn.connect(syn.payload.dst, syn.dport)
-    # Change the sequence number so that we can test it
-    conn.seq = syn.seq
-    return listener, conn
-
-def check_mostly_same(pkt1, pkt2):
-    assert pkt1.seq == pkt2.seq
-    assert pkt1.ack == pkt2.ack
-    assert pkt1.sprintf("%TCP.flags%") == pkt2.sprintf("%TCP.flags%")
-    if not hasattr(pkt1, 'load'):
-        pkt1.load = None
-    if not hasattr(pkt2, 'load'):
-        pkt2.load = None
-    assert pkt1.load == pkt2.load
-
 def test_send_push_ack():
+    """
+    When we send a message, it should look the same as in the PCAP log
+    """
     packet_log = rdpcap("test/inputs/localhost-wget.pcap")
     listener, conn = create_session(packet_log)
 
@@ -81,7 +66,10 @@ def test_send_push_ack():
     check_mostly_same(our_push_ack, push_ack)
 
 
-def test_fin_ack():
+def test_fin_ack_client():
+    """
+    When we run conn.close(), it should end the connection properly.
+    """
     packet_log = rdpcap("test/inputs/tiniest-session.pcap")
     listener, conn = create_session(packet_log)
 
@@ -97,6 +85,173 @@ def test_fin_ack():
     check_mostly_same(our_ack, our_ack_log)
 
     assert conn.state == "TIME-WAIT"
+
+
+def test_recv_one_packet():
+    """
+    conn.recv() should get the contents of one packet
+    """
+    packet_log = rdpcap("test/inputs/localhost-wget.pcap")
+    listener, conn = create_session(packet_log)
+
+    _, syn_ack, _, push_ack = packet_log[:4]
+
+    listener.dispatch(syn_ack)
+    payload = str(push_ack.payload.payload.payload)
+    conn.send(payload)
+
+    # Check that the PUSH/ACK sequence is the same
+    check_replay(listener, conn, packet_log[4:7])
+
+    # Check that recv() actually works
+    assert conn.recv() == str(packet_log[5].payload.payload.payload)
+
+def test_recv_many_packets_in_order():
+    """
+    We should be able to put multiple packets together, if they come in order
+    """
+    packet_log = rdpcap("test/inputs/wget-36000-nums.pcap")
+    listener, conn = create_session(packet_log)
+    _, syn_ack, _, push_ack = packet_log[:4]
+
+    listener.dispatch(syn_ack)
+    payload = str(push_ack.payload.payload.payload)
+    conn.send(payload)
+
+    check_replay(listener, conn, packet_log[4:], check=False)
+
+    # Check that the contents of the packet is right
+    recv = conn.recv()
+    assert recv[-36001:-1]  == "1234567890" * 3600
+
+def test_recv_many_packets_out_of_order():
+    """
+    We should be able to put multiple packets together, if they come
+    out of order and are repeated.
+    """
+    packet_log = rdpcap("test/inputs/wget-36000-nums.pcap")
+    listener, conn = create_session(packet_log)
+    _, syn_ack, _, push_ack = packet_log[:4]
+
+    listener.dispatch(syn_ack)
+    payload = str(push_ack.payload.payload.payload)
+    conn.send(payload)
+
+    p1, p2, p3  = packet_log[5], packet_log[7], packet_log[8]
+
+    # Send the packets out of order and repeated
+    listener.dispatch(p2)
+    listener.dispatch(p3)
+    listener.dispatch(p2)
+    listener.dispatch(p1) # Right
+    listener.dispatch(p3)
+    listener.dispatch(p2) # Right
+    listener.dispatch(p3) # Right
+
+    # Check that the contents of the packet is right
+    # This is a good test because one of the packets starts with a 6 or
+    # something
+    recv = conn.recv()
+    assert recv[-36001:-1]  == "1234567890" * 3600
+
+def test_bind_handshake():
+    """
+    We can do a handshake when we're the server (someone else initiates the
+    SYN).
+    """
+    packet_log = rdpcap("test/inputs/tiniest-session.pcap")
+    syn, syn_ack, ack, client_fin_ack, server_fin_ack, client_ack = packet_log
+
+    listener = MockListener()
+    conn = TCPSocket(listener)
+    conn.seq = syn_ack.seq
+    conn.bind(syn.payload.dst, syn.dport)
+
+    listener.dispatch(syn)
+    listener.dispatch(ack)
+
+    assert len(listener.received_packets) == 1
+    assert conn.state == "ESTABLISHED"
+    check_mostly_same(listener.received_packets[0], syn_ack)
+
+
+def test_tiny_session_server():
+    """
+    Test the whole tiny session, from a server POV.
+    """
+    packet_log = rdpcap("test/inputs/tiniest-session.pcap")
+    syn, syn_ack = packet_log[:2]
+
+    listener = MockListener()
+    conn = TCPSocket(listener)
+    conn.seq = syn_ack.seq
+
+    conn.bind(syn.payload.dst, syn.dport)
+
+    check_replay(listener, conn, packet_log)
+
+
+def test_closes_on_reset():
+    """The connection should close when it receives a RST packet"""
+    listener = MockListener()
+
+    packet_log = rdpcap("test/inputs/tiniest-session.pcap")
+    listener, conn = create_session(packet_log)
+    syn_ack = packet_log[1]
+
+    listener.dispatch(syn_ack)
+
+    reset = syn_ack.copy()
+    reset.payload.payload.flags = "R"
+    reset.seq += 1
+    listener.dispatch(reset)
+
+    assert conn.state == "CLOSED"
+
+
+def test_sends_reset():
+    """The connection should send a RST packet when it's closed."""
+    listener = MockListener()
+
+    packet_log = rdpcap("test/inputs/tiniest-session.pcap")
+    listener, conn = create_session(packet_log)
+    conn.state = "CLOSED"
+
+    listener.dispatch(syn_ack)
+
+    last_packet = listener.received_packets[-1]
+    assert last_packet
+    assert conn.state == "CLOSED"
+
+
+def create_session(packet_log):
+    """
+    Helper function to create a client session from a PCAP log. Sets up the
+    sequence numbers to match with the sequence numbers in the PCAP.
+    """
+    listener = MockListener()
+    syn = packet_log[0]
+    listener.source_port = syn.sport - 1
+    conn = TCPSocket(listener)
+    conn.connect(syn.payload.dst, syn.dport)
+    # Change the sequence number so that we can test it
+    conn.seq = syn.seq
+    return listener, conn
+
+
+def check_mostly_same(pkt1, pkt2):
+    """
+    Checks that the important things are the same (seq, ack, TCP flags, packet
+    contents)
+    """
+    assert pkt1.seq == pkt2.seq
+    assert pkt1.ack == pkt2.ack
+    assert pkt1.sprintf("%TCP.flags%") == pkt2.sprintf("%TCP.flags%")
+    if not hasattr(pkt1, 'load'):
+        pkt1.load = None
+    if not hasattr(pkt2, 'load'):
+        pkt2.load = None
+    assert pkt1.load == pkt2.load
 
 
 def check_replay(listener, conn, packet_log, check=True):
@@ -121,106 +276,4 @@ def check_replay(listener, conn, packet_log, check=True):
         check_mostly_same(ours, actual)
 
 
-def test_recv_one_packet():
-    packet_log = rdpcap("test/inputs/localhost-wget.pcap")
-    listener, conn = create_session(packet_log)
-
-    _, syn_ack, _, push_ack = packet_log[:4]
-
-    listener.dispatch(syn_ack)
-    payload = str(push_ack.payload.payload.payload)
-    conn.send(payload)
-
-    # Check that the PUSH/ACK sequence is the same
-    check_replay(listener, conn, packet_log[4:7])
-
-    # Check that recv() actually works
-    assert conn.recv() == str(packet_log[5].payload.payload.payload)
-
-def test_recv_many_packets_in_order():
-    packet_log = rdpcap("test/inputs/wget-36000-nums.pcap")
-    listener, conn = create_session(packet_log)
-    _, syn_ack, _, push_ack = packet_log[:4]
-
-    listener.dispatch(syn_ack)
-    payload = str(push_ack.payload.payload.payload)
-    conn.send(payload)
-
-    check_replay(listener, conn, packet_log[4:], check=False)
-
-    # Check that the contents of the packet is right
-    recv = conn.recv()
-    assert recv[-36001:-1]  == "1234567890" * 3600
-
-def test_recv_many_packets_out_of_order():
-    packet_log = rdpcap("test/inputs/wget-36000-nums.pcap")
-    listener, conn = create_session(packet_log)
-    _, syn_ack, _, push_ack = packet_log[:4]
-
-    listener.dispatch(syn_ack)
-    payload = str(push_ack.payload.payload.payload)
-    conn.send(payload)
-
-    p1, p2, p3  = packet_log[5], packet_log[7], packet_log[8]
-
-    # Send the packets out of order and repeated
-    listener.dispatch(p2)
-    listener.dispatch(p3)
-    listener.dispatch(p2)
-    listener.dispatch(p1) # Right
-    listener.dispatch(p3)
-    listener.dispatch(p2) # Right
-    listener.dispatch(p3) # Right
-
-    # Check that the contents of the packet is right
-    recv = conn.recv()
-    assert recv[-36001:-1]  == "1234567890" * 3600
-
-def test_bind_handshake():
-    packet_log = rdpcap("test/inputs/tiniest-session.pcap")
-    syn, syn_ack, ack, client_fin_ack, server_fin_ack, client_ack = packet_log
-
-    listener = MockListener()
-    conn = TCPSocket(listener)
-    conn.seq = syn_ack.seq
-    conn.bind(syn.payload.dst, syn.dport)
-
-    listener.dispatch(syn)
-    listener.dispatch(ack)
-
-    assert len(listener.received_packets) == 1
-    assert conn.state == "ESTABLISHED"
-    check_mostly_same(listener.received_packets[0], syn_ack)
-
-def test_tiny_session_server():
-    """
-    Test the whole tiny session, from a server POV.
-    """
-    packet_log = rdpcap("test/inputs/tiniest-session.pcap")
-    syn, syn_ack = packet_log[:2]
-
-    listener = MockListener()
-    conn = TCPSocket(listener)
-    conn.seq = syn_ack.seq
-
-    conn.bind(syn.payload.dst, syn.dport)
-
-
-    check_replay(listener, conn, packet_log)
-
-def test_reset():
-    listener = MockListener()
-
-    packet_log = rdpcap("test/inputs/tiniest-session.pcap")
-    listener, conn = create_session(packet_log)
-    syn_ack = packet_log[1]
-
-    listener.dispatch(syn_ack)
-
-    reset = syn_ack.copy()
-    reset.payload.payload.flags = "R"
-    reset.seq += 1
-    listener.dispatch(reset)
-
-    assert conn.state == "CLOSED"
 
