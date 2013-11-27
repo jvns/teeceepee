bd293bac7ec9cd41d59b1452ed4731292cefaa89
Julia Evans <julia@jvns.ca>
2013-11-27 16:52:51 -0500

    Make curl example more robust

--- examples/curl.py
+++ examples/curl.py
@@ -1,6 +1,7 @@
 # WARNING: This does not work yet
 import sys
 import socket
+import time
 from scapy.all import srp, Ether, ARP
 sys.path.insert(0, ".")
 from teeceepee.tcp import TCPSocket
@@ -13,6 +14,7 @@ def arp_spoof():
     try:
         for _ in range(4):
             srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(psrc=FAKE_IP, hwsrc=MAC_ADDR), verbose=0)
+            time.sleep(0.1)
         listener = TCPListener(FAKE_IP)
     except socket.error:
         # Are you sure you're running as root?
@@ -20,20 +22,35 @@ def arp_spoof():
         print "ERROR: You need to run this script as root."
         sys.exit(1)
 
+def parse(url):
+    """
+    Parses an URL and returns the hostname and the rest
+    """
+    # We *definitely* don't support https
+    if 'https' in url:
+        print "We don't support https."
+        sys.exit(1)
+    if '://' in url:
+        url = url.split('://')[1]
 
-def get_page(hostname):
-    print "Getting", hostname
-    payload = "GET / HTTP/1.0\r\nHost: %s\r\n\r\n" % hostname
+    parts = url.split('/')
+    hostname = parts[0]
+    path = '/' + '/'.join(parts[1:])
+    return hostname, path
+
+def get_page(url):
+    print "Getting", url
+    hostname, path = parse(url)
+    request = "GET %s HTTP/1.0\r\nHost: %s\r\n\r\n" % (path, hostname)
     listener = TCPListener(FAKE_IP)
     conn = TCPSocket(listener)
 
     conn.connect(hostname, 80)
-    conn.send(payload)
-    time.sleep(3)
+    conn.send(request)
+    time.sleep(5)
     data = conn.recv()
     conn.close()
-    time.sleep(3)
-
+    time.sleep(1)
     return data
 
 if __name__ == "__main__":
